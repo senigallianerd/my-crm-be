5605c1c0b254554a8da361848ef0183409bee21c
Francesco Cini <francesco.cini@gruppometa.it>
2021-02-20 02:09:28 +0100

    upload

--- api/conf.php
+++ api/conf.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: http://localhost:4200', false);
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization'");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization");
 
 $servername = "localhost";
 $username = "root";
