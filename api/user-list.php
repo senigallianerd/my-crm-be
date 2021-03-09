5d83eb7ffe755c5003ceac00c0b93ead0a483d7c
Francesco Cini <francesco.cini@gruppometa.it>
2021-03-09 14:41:32 +0100

    semplificata query

--- api/user-list.php
+++ api/user-list.php
@@ -4,7 +4,7 @@ include "./conf/conf.php";
 
 $users = [];
 
-$sql = "SELECT e.id,e.name, e.surname, e.age, e.link, m.surname as linkSurname FROM Users e LEFT JOIN Users m ON m.id = e.userId ORDER BY e.id";
+$sql = "SELECT * FROM Users";
 $result = $con->query($sql);
 if ($result->num_rows > 0) {
  while($row = $result->fetch_assoc()) {
