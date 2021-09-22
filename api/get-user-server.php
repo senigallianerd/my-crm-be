37ccf15204f4f2552a88b60da5ae7f4bff42d8e9
Francesco Cini <francesco.cini@gruppometa.it>
2021-09-22 11:05:00 +0200

    gestione server side utenti

---
+++ api/get-user-server.php
@@ -0,0 +1,59 @@
+<?php
+ 
+include "./conf/conf.php";
+ 
+$params = json_decode(file_get_contents('php://input'), true);
+$users = [];
+
+ $columns = array(
+ 0 => 'id',
+ 1 => 'cognome', 
+ 2 => 'nome'
+ );
+
+
+ $where_condition = $sqlTot = $sqlRec = "";
+ 
+ if( !empty($params['search']['value']) ) {
+ $where_condition .= " WHERE ";
+ $where_condition .= " ( nome LIKE '%".$params['search']['value']."%' ";    
+ $where_condition .= " OR cognome LIKE '%".$params['search']['value']."%' )";
+ }
+ 
+ $sql_query = " SELECT * FROM Users ";
+ $sqlTot .= $sql_query;
+ $sqlRec .= $sql_query;
+ 
+ if(isset($where_condition) && $where_condition != '') {
+ 
+ $sqlTot .= $where_condition;
+ $sqlRec .= $where_condition;
+ }
+ 
+ $sqlRec .=  " ORDER BY ". $columns[$params['order'][0]['column']]."   ".$params['order'][0]['dir']."  LIMIT ".$params['start']." ,".$params['length']." ";
+ 
+ $queryTot = mysqli_query($con, $sqlTot) or die("Database Error:". mysqli_error($con));
+ 
+ $totalRecords = mysqli_num_rows($queryTot);
+ 
+ $result = $con->query($sqlRec);
+ if ($result->num_rows > 0) {
+ while($row = $result->fetch_assoc()) {
+  array_push($users,$row);
+ }
+}
+ 
+ $json_data = array(
+ "draw"            => intval( $params['draw'] ),   
+ "recordsTotal"    => intval( $totalRecords ),  
+ "recordsFiltered" => intval($totalRecords),
+ "data"            => $users
+ );
+ 
+ echo json_encode($json_data);
+$con->close();
+?>
+
+
+
+
