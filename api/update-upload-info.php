40306dd8f093ac9979462f64ba2296dbd6891ad0
Francesco Cini <francesco.cini@gruppometa.it>
2021-11-19 14:55:13 +0100

    aggiunta servizio update doc

---
+++ api/update-upload-info.php
@@ -0,0 +1,39 @@
+<?php
+
+include "./conf/conf.php";
+
+$data = json_decode(file_get_contents('php://input'), true);
+$id = $data["id"];
+$numero = $data["numero"];
+$ramo = $data["ramo"];
+$sottotipoDoc = $data["sottotipoDoc"];
+$targa = $data["targa"];
+$date = $data["singleDate"];
+$frazionamentoSemestrale = $data["frazionamentoSemestrale"];
+$premioAnnuale =$data["premioAnnuale"];
+$premioRata = $data["premioRata"];
+$fattura = $data["fattura"];
+$note = $data["noteDoc"];
+$fileName = $data["fileName"];
+
+$sql = "UPDATE `Docs` SET `numero` = '".$numero."', 
+                                `ramo` = '".$ramo."', 
+                                `sottotipoDoc` = '".$sottotipoDoc."', 
+                                `targa` = '".$targa."', 
+                                `data` = '".$date."', 
+                                `frazionamentoSemestrale` = '".$frazionamentoSemestrale."', 
+                                `premioAnnuale` = '".$premioAnnuale."', 
+                                `premioRata` = '".$premioRata."', 
+                                `fattura` = '".$fattura."', 
+                                `fileName` = '".$fileName."', 
+                                `note` =  '".$note."' WHERE `Docs`.`id` = ".$id.";";
+$result = $con->query($sql);
+
+echo json_encode($result);
+
+$con->close();
+
+?>
+
+
+
