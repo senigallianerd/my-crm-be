03e01b7691de071609e0debd2d335e663fc39e51
Francesco Cini <francesco.cini@gruppometa.it>
2022-01-28 12:10:20 +0100

    aggiunta file

---
+++ mail-report_lore.php
@@ -0,0 +1,165 @@
+<?php
+if($_GET['email']){
+  ob_start();
+?>
+
+<html>
+<head>
+<title>Mail Report</title>
+<style>
+      th{
+        background:#7777772e;
+      }
+      table {
+        color: #333;
+        background: white;
+        border: 1px solid grey;
+        font-size: 12pt;
+        border-collapse: collapse;
+      }
+      table thead th,
+      table tfoot th {
+        color: #777;
+        background: rgba(0,0,0,.1);
+      }
+      table th,
+      table td {
+        padding: .5em;
+        border: 1px solid lightgrey;
+      }
+
+</style>
+</head>
+<body>
+
+<?php
+
+include "./conf/conf.php";
+
+$docs = [];
+
+$monthToValue = array("1"=>"Gennaio", "2"=>"Febbraio", "3"=>"Marzo", "
+      4"=>"Aprile", "5"=>"Maggio", "6"=>"Giugno",
+      "7"=>"Luglio", "8"=>"Agosto", "9"=>"Settembre",
+      "10"=>"Ottobre", "11"=>"Novembre", "12"=>"Dicembre" );
+
+$month = $_GET['month'];
+$year = $_GET['year'];
+
+if(!$month)
+    $month=date("m");
+
+if(!$year)
+    $year=date("Y");
+
+// se siamo a Dicembre devo inserire il mese di Gennaio
+$nextMonth = $month < 12 ? $month+1 : 1;
+
+// se siamo a Dicembre devo inserire l'anno successivo
+$correctYear = $month < 12 ? $year : $year+1;
+
+echo '<br> Risultati di ricerca del mese: <b>' .$monthToValue[intval($nextMonth)]. '</b> anno: <b>' .$correctYear. '</b>';
+echo '<br><br>';
+
+$sql = "SELECT nome,cognome,numero,targa,frazionamento,premioRata,data, Docs.note as noteDoc FROM Docs,Users WHERE MONTH(data) = ".$nextMonth." and YEAR(data) = ".$correctYear." and UserId = Users.id ORDER BY DATE(data) ASC";
+
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+ while($row = $result->fetch_assoc()) {
+  array_push($docs,$row);
+ }
+}
+
+if(empty($docs)){
+  echo "Nessun risultato trovato";
+}
+else{
+  ?>
+
+<table>
+  <tr>
+    <th>Nome</th>
+    <th>Cognome</th>
+    <th>Polizza</th>
+    <th>Targa</th>
+    <th>Note</th>
+    <th>Frazionamento</th>
+    <th>Premio Rata</th>
+    <th>Scadenza</th>
+  </tr>
+
+  <?php
+
+  foreach($docs as $doc) {
+    $premioRata = $doc['premioRata'] ? $doc['premioRata'] . ' &euro;' : '';
+    echo "<tr>
+              <td>".$doc['nome']."</td>
+              <td>".$doc['cognome']."</td>
+              <td>".$doc['numero']."</td>
+              <td>".$doc['targa']. "</td>
+              <td>".addSlashes($doc['noteDoc']). "</td>
+              <td>".$doc['frazionamento']. "</td>
+              <td>".$premioRata."</td>
+              <td>".date("d/m/Y", strtotime($doc['data']))."</td>
+          </tr>";
+}
+}
+
+
+$con->close();
+
+function loreSendMail($text){
+	$toEmail ="lorenzo@posta.biz";
+    $subject = "Prossime scadenze";
+    $body = $text;
+    //$headers = "From: noreplay@assicurazionisilli.com\r\n";
+    $headers = implode("\r\n", [
+    'From: Assicurazioni Silli <noreplay@assicurazionisilli.com>',
+    'Reply-To: noreplay@assicurazionisilli.com',
+    'MIME-Version: 1.0',
+    'Content-Type: text/html; charset=ISO-8859-1',
+    'X-Mailer: PHP/' . PHP_VERSION
+	]);
+    $cc = null;
+    $bcc = null;
+    //$return_path = "noreplay@assicurazionisilli.com";
+    //send the email using IMAP
+    $a = imap_mail($toEmail, $subject, $body, $headers, $cc, $bcc, $return_path);
+    echo "Email sent!<br />" .$a;
+}
+
+function sendMail($text){
+  $url = 'https://www.frakorn.it/mail-server/send.php';
+  $data = array('sendTo' => 'lorenzo@posta.biz', 'message' => $text,'subject' => 'MAIL REPORT','fromName' => 'CRM');
+  $options = array(
+          'http' => array(
+          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method'  => 'POST',
+          'content' => http_build_query($data)
+          )
+  );
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  if ($result === FALSE) { 
+          echo "Error Sending Mail"; 
+  }
+  echo "<br> Email inviata correttamente";
+}
+?>
+</table>
+
+</body>
+</html>
+
+<?php
+// ottengo tutto il contenuto HTML della mia pagina
+$html = ob_get_contents();
+
+if($_GET['email']=='true')
+  //sendMail($html);
+	loreSendMail($html);
+
+ob_end_flush();
+}
+?>
