2887a35f17ae5b92cbd59739d13e4d8a9b131c99
Francesco Cini <francesco.cini@gruppometa.it>
2022-03-04 16:09:47 +0100

    modificato servizio cancellazione file con copia in cartella trash

--- api/delete-policy.php
+++ api/delete-policy.php
@@ -9,15 +9,25 @@ $result = $con->query($strSQL);
 $row = $result->fetch_assoc();
 $fileName = $row['fileName'];
 
-$path='uploads/'.$fileName;
+$pathSource='uploads/'.$fileName;
+$pathDestination='uploads/trash/'.$fileName;
+
 
 $sql = "DELETE FROM Docs  WHERE id=".$policyId;
 $result = $con->query($sql);
 
-if(unlink($path) && $result)
-echo json_encode($result);
+if (!copy($pathSource, $pathDestination)) {
+    // errore nella copia
+    echo json_encode($result);
+}
+else{
+    // se tutto ok cancella file orig
+    unlink($pathSource);
+    echo json_encode($result);
+}
+
+
 
 $con->close();
 
 ?>
-
